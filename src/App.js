b7ce4d1909b8ea9654728a8fb475eaa4ca0890ee
Devil39 <fenil.jain2018@vitstudent.ac.in>
2019-08-03 19:39:35 +0530

    something

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {Container} from 'react-bootstrap';
 import {Row} from 'react-bootstrap';
 import {Col} from 'react-bootstrap';
 import {Button} from 'react-bootstrap';
-import { interfaceExtends } from '@babel/types';
 
 class App extends React.Component
 {
@@ -17,33 +16,38 @@ class App extends React.Component
       email: "",
       contactno: "",
       domains: [],
-      ohter: "",
-      disabled: ""
+      other: "",
+      disabled: "",
+      mlcheck: false,
+      echeck: false,
+      wcheck: false,
+      acheck: false,
+      ocheck: false
     };
   }
   submit=()=>{
-    //console.log("Submit="+this.state.name);
-    console.log("Submitted");
-    console.log(this.state);
-    fetch('',{
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-          name: this.state.name,
-          regNo: this.state.regno,
-          email: this.state.email,
-          contactNo: this.state.contactno,
-          InitDomain: this.state.domains,
-          other: this.state.other
-      })
-    })/*.then(res=>{console.log(res);console.log(res.json());var re=res.json();return re;})*/
-    .then(res=>res.json())
-    .then(data=>{
-        console.log("data");
-      }).catch(()=>
-      {
-        console.log("Error!");
-      });
+    console.log("Submit="+JSON.stringify(this.state));
+    // console.log("Submitted");
+    // console.log(this.state);
+    // fetch('',{
+    //   method: 'post',
+    //   headers: {'Content-Type': 'application/json'},
+    //   body: JSON.stringify({
+    //       name: this.state.name,
+    //       regNo: this.state.regno,
+    //       email: this.state.email,
+    //       contactNo: this.state.contactno,
+    //       InitDomain: this.state.domains,
+    //       other: this.state.other
+    //   })
+    // })/*.then(res=>{console.log(res);console.log(res.json());var re=res.json();return re;})*/
+    // .then(res=>res.json())
+    // .then(data=>{
+    //     console.log("data");
+    //   }).catch(()=>
+    //   {
+    //     console.log("Error!");
+    //   });
   }
   changeregno=(event)=>{
     this.setState({regno: event.target.value});
@@ -81,6 +85,21 @@ class App extends React.Component
     });
     //console.log(this.state.name);
   }
+  mlcheckchange=(event)=>{
+    this.setState({mlcheck: !this.state.mlcheck});
+  }
+  echeckchange=(event)=>{
+    this.setState({echeck: !this.state.echeck});
+  }
+  acheckchange=(event)=>{
+    this.setState({acheck: !this.state.acheck});
+  }
+  ocheckchange=(event)=>{
+    this.setState({ocheck: !this.state.ocheck});
+  }
+  wcheckchange=(event)=>{
+    this.setState({wcheck: !this.state.wcheck});
+  }
   render(){
     return(
       <div className="body"> 
@@ -105,20 +124,20 @@ class App extends React.Component
                 <input placeholder="Your response" />
                 <label>Interested Domain(s)<span> *</span></label>
                 <br/>
-                <div id="checkStyle"><input type="checkbox" class="checkBox" id="M"/><label id="oplabel">Machine Learning</label></div>
-                <div id="checkStyle"><input type="checkbox" class="checkBox" id="W"/><label id="oplabel">Web Development</label></div>
-                <div id="checkStyle"><input type="checkbox" class="checkBox" id="A"/><label id="oplabel">Android Development</label></div>
-                <div id="checkStyle"><input type="checkbox" class="checkBox" id="E"/><label id="oplabel">Electronics</label></div>
-                <div id="checkStyle"><input type="checkbox" class="checkBox" id="O"/>
+                <div id="checkStyle"><input type="checkbox" checked={this.state.checked} className="checkBox" id="M" onChange={this.mlcheckchange}/><label id="oplabel">Machine Learning</label></div>
+                <div id="checkStyle"><input type="checkbox" className="checkBox" id="W"/><label id="oplabel" onChange={this.wcheckchange}>Web Development</label></div>
+                <div id="checkStyle"><input type="checkbox" className="checkBox" id="A"/><label id="oplabel" onChange={this.acheckchange}>Android Development</label></div>
+                <div id="checkStyle"><input type="checkbox" className="checkBox" id="E"/><label id="oplabel" onChange={this.echeckchange}>Electronics</label></div>
+                <div id="checkStyle"><input type="checkbox" className="checkBox" id="O" onChange={this.ocheckchange}/>
                   <label id="oplabel">Other: </label>
                   <input id="others"></input>
                 </div>
-                <Button variant="primary" id="button">Submit</Button>
+                <Button variant="primary" id="button" onClick={()=>this.submit()}>Submit</Button>
               </form>
             </Col>
             <Col md={3}></Col>
           </Row>
-          <img src={"ieee.png"} id="ieee"></img>
+          <img src={"ieee.png"} id="ieee" alt=""></img>
         </Container>
       </div>
     );
