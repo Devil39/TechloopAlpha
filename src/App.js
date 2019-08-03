b9c3eab33748ad0bba27b5f701d983790118e6ba
Devil39 <fenil.jain2018@vitstudent.ac.in>
2019-08-03 17:42:04 +0530

    Init push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as bs from 'bootstrap/dist/css/bootstrap.css';
+import {Container} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body"> 
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"></link>
+      <Container id="main-container">
+        <Row id="rows">
+          <Col md={3}></Col>
+          <Col md={6} id="form-div">
+            <img id="techloopLogo" src={"image.jpg"} alt="" />
+            <form>
+              <label>Name<span> *</span></label>
+              <br/>
+              <input className="textField" placeholder="Your response" />
+              <label>Registration No.<span> *</span></label>
+              <br/>
+              <input className="textField" placeholder="Your response" />
+              <label>Email<span> *</span></label>
+              <br/>
+              <input className="textField" placeholder="Your response" />
+              <label>Contact No.<span> *</span></label>
+              <br/>
+              <input className="textField" placeholder="Your response" />
+              <label>Interested Domain(s)<span> *</span></label>
+              <br/>
+              <input className="checkBox" type="checkbox"/>
+              <Button variant="primary" id="button">Submit</Button>
+            </form>
+          </Col>
+          <Col md={3}></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
